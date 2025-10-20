f90640dc9e4e3d6ce895241ec2b4d11668236589
simonecontidev <barriotropical@gmail.com>
2025-10-20 15:26:46 +0200

    install three js, hero section
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,36 +1,66 @@
-import { useRef } from "react";
-import { AnimatedTextLines } from "../components/AnimateTextLines";
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "../components/Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 
 const Hero = () => {
-    const contextRef = useRef(null);
-    const headerRef = useRef(null);
-    const aboutText = `I help growinf brands and company gain an unfair advantage through premium results driven webs/app`;
+  const isMobile = useMediaQuery({ maxWidth: 853 });
+  const text = `Building clean, animated, 
+  and meaningful web experiences.`;
 
   return (
-    <section id="home" 
-    className="flex flex-col justify-end min-h-screen">
-        <div ref={contextRef}>
-            <div style={{clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%"} }>
-                <div ref={headerRef} className="flex flex-col justify-center gap-12 pt-16 sm:gap-16">
-                    <p className="text-sm font-light tracliking-[0.5rem] uppercase px-10 text-black">404 no bugs found</p>
-                <div className="px-10">
-                    <h1 className="flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive sm:gap-16 md:block text-5xl">Simone Conti</h1>
-                    </div>
-                </div>
-            </div>
-
-            <div className="relative px-10 text-black">
-                <div className="absolute inset-x-0 border-t-2"></div>
-                <div className="py-12 sm:py-16 text-end">
-                    <AnimatedTextLines 
-                    text={aboutText} 
-                    className="font-light uppercase value-text-responsive">    
-                    </AnimatedTextLines>
-                </div>
-            </div>
-        </div>
+    <section id="home" className="flex flex-col justify-end min-h-screen">
+      <AnimatedHeaderSection
+        subTitle={"404 No Bugs Found"}
+        title={"Simone Conti"}
+        text={text}
+        textColor={"text-black"}
+      />
+      <figure
+        className="absolute inset-0 -z-50"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas
+          shadows
+          camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
+        >
+          <ambientLight intensity={0.5} />
+          <Float speed={0.5}>
+            <Planet scale={isMobile ? 0.7 : 1} />
+          </Float>
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 3, 1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[-5, -1, -1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[10, 1, 0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas>
+      </figure>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
