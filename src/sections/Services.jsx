4d3ded38024b737a02f3e8ea63baf38c06adaf56
simonecontidev <barriotropical@gmail.com>
2025-10-20 16:01:21 +0200

    Service section
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,20 +1,81 @@
-import AnimatedHeaderSection from "../components/AnimatedHeaderSection"
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { servicesData } from "../constants";
+import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Services = () => {
+  const text = `I create dynamic user experiences that move with purpose —
+reveals, parallax, scroll stories, 
+and subtle transitions that give life to your brand.`;
+  const serviceRefs = useRef([]);
+  const isDesktop = useMediaQuery({ minWidth: "48rem" }); //768px
+  useGSAP(() => {
+    serviceRefs.current.forEach((el) => {
+      if (!el) return;
 
-    const text = `I build scalable interfaces and pixel-perfect components.
-From landing pages to full-stack apps — clean code, SEO-ready, and built for performance.`
+      gsap.from(el, {
+        y: 200,
+        scrollTrigger: {
+          trigger: el,
+          start: "top 80%",
+        },
+        duration: 1,
+        ease: "circ.out",
+      });
+    });
+  }, []);
   return (
     <section id="services" className="min-h-screen bg-black rounded-t-4xl">
       <AnimatedHeaderSection
-      subTitle={"One pixel at time"}
-      title={"Service"}
-      text={text}
-      textColor={"text-white"}
-      withScrollTrigger={true}
+        subTitle={"Behind the scene, Beyond the screen"}
+        title={"Service"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
       />
+      {servicesData.map((service, index) => (
+        <div
+          ref={(el) => (serviceRefs.current[index] = el)}
+          key={index}
+          className="sticky px-10 pt-6 pb-12 text-white bg-black border-t-2 border-white/30"
+          style={
+            isDesktop
+              ? {
+                  top: `calc(10vh + ${index * 5}em)`,
+                  marginBottom: `${(servicesData.length - index - 1) * 5}rem`,
+                }
+              : { top: 0 }
+          }
+        >
+          <div className="flex items-center justify-between gap-4 font-light">
+            <div className="flex flex-col gap-6">
+              <h2 className="text-4xl lg:text-5xl">{service.title}</h2>
+              <p className="text-xl leading-relaxed tracking-widest lg:text-2xl text-white/60 text-pretty">
+                {service.description}
+              </p>
+              <div className="flex flex-col gap-2 text-2xl sm:gap-4 lg:text-3xl text-white/80">
+                {service.items.map((item, itemIndex) => (
+                  <div key={`item-${index}-${itemIndex}`}>
+                    <h3 className="flex">
+                      <span className="mr-12 text-lg text-white/30">
+                        0{itemIndex + 1}
+                      </span>
+                      {item.title}
+                    </h3>
+                    {itemIndex < service.items.length - 1 && (
+                      <div className="w-full h-px my-2 bg-white/30" />
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
