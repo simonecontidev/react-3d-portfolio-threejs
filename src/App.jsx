8704bc8555bf731a74fdf578670ba916a1a29547
simonecontidev <barriotropical@gmail.com>
2025-10-20 16:16:43 +0200

    work section
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ServiceSummary from './sections/ServiceSummary'
 import Services from './sections/Services'
 import ReactLenis from 'lenis/react'
 import About from './sections/About'
+import Work from './sections/Work'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <ServiceSummary/>
       <Services/>
       <About/>
+      <Work/>
     </ReactLenis>
   )
 }
